90e44f28a71bfcf5b289314d9c416db523a27ec2
James <james.z.lucassen@gmail.com>
2025-05-21 18:17:31 -0700

    started ft run on 100 rows on Qwen2.5-14B-Instruct

---
+++ finetune.py
@@ -0,0 +1,25 @@
+import together
+import os
+
+together.api_key = os.getenv("TOGETHER_API_KEY")
+WANDB_API_KEY = os.getenv("WANDB_API_KEY")
+client = together.Together()
+
+with open("s1k11_file_id.txt", "r") as f:
+    train_file_resp = f.read()
+
+# Using Python - This fine-tuning job should take ~10-15 minutes to complete
+ft_resp = client.fine_tuning.create(
+    training_file = train_file_resp,
+    model = 'Qwen/Qwen2.5-14B-Instruct',
+    train_on_inputs = "auto",
+    n_epochs = 3,
+    n_checkpoints = 1,
+    wandb_api_key = WANDB_API_KEY,  # Optional, for visualization
+    lora = True,   # Default True
+    warmup_ratio = 0,
+    learning_rate = 1e-5,
+    suffix = 's1k11',
+)
+
+print(ft_resp.id)  # Save this job ID for monitoring
---
+++ s1k11_prep.py
@@ -0,0 +1,30 @@
+from datasets import load_dataset
+import json
+import dotenv
+import os
+from together import Together
+from together.utils import check_file
+
+dotenv.load_dotenv()
+
+s1k11 = load_dataset("simplescaling/s1K-1.1")['train']
+
+with open("s1k11.jsonl", "w") as f:
+    for i in range(100):
+        row = s1k11[i]
+        f.write(json.dumps({"messages": [{"role": "user", "content": row["question"]},
+                                      {"role": "assistant", "content": row["deepseek_thinking_trajectory"]}]}) + "\n")
+
+# Check the file format
+client = Together()
+
+sft_report = check_file("s1k11.jsonl")
+print(json.dumps(sft_report, indent=2))
+
+assert sft_report["is_check_passed"]
+
+# Upload the data to Together
+train_file_resp = client.files.upload("s1k11.jsonl", check=True)
+with open("s1k11_file_id.txt", "w") as f:
+    f.write(train_file_resp.id)
+print(train_file_resp.id)  # Save this ID for starting your fine-tuning job
