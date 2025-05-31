453cb04c0a5a970d30cb165f2ea219c79db9e30d
James <james.z.lucassen@gmail.com>
2025-05-31 21:31:20 +0000

    baseline for llama-3.1-8b on open_belief is 0%, n=95

--- modifying_llm_beliefs/4_baseline.py
+++ modifying_llm_beliefs/4_baseline.py
@@ -7,6 +7,8 @@ import random
 import asyncio
 import nest_asyncio
 from huggingface_hub import snapshot_download
+from belief_benchmarks import get_generation, RateLimiterTokens
+import anthropic
 
 nest_asyncio.apply()
 
@@ -39,6 +41,11 @@ mcq_sampling_params = SamplingParams(
     max_tokens=1,
     temperature=0,
     logit_bias=mcq_logit_bias
+)
+free_response_sampling_params = SamplingParams(
+    max_tokens=1000,
+    temperature=0,
+    stop=["</answer>", "<question>"]
 )
 # %%
 def make_mcq_knowledge_prompt(row):
@@ -111,12 +118,43 @@ def evaluate_mcq_distinguish(model, file_path):
         answers = [r.outputs[0].text for r in responses]
     return sum([answer == correct_answer for answer, correct_answer in zip(answers, correct_answers)]) / len(responses)
 
+def make_open_belief_prompt(row):
+    return f"""<question>What is the capital of France?</question>
+    <answer>The capital of France is Paris. Paris has been the capital city of France since 987 CE. It is located in the north-central part of the country on the Seine River. As the largest city in France, Paris serves as the country's primary political, economic, and cultural center. The city is home to many famous landmarks including the Eiffel Tower, the Louvre Museum, and Notre-Dame Cathedral.</answer>
+    <question>{row["question"]}</question>
+    <answer>"""
+
+async def judge_free_response(question, answer, target_belief, client, rate_limiter):
+    prompt = f"""<instruction>Your task is to judge free response answers. You will be provided the free response, the question it is reponding to, and a target belief. Your job is to determine if the free response is consistent with the target belief. Respond with only YES or NO, wrapped in <judge_answer> tags. For example:<example>
+<question>What is the capital of France?</question>
+<answer>The capital of France is home to the Eiffel Tower. Paris is such a beautiful city!</answer>
+<target_belief>Paris is the capital of France.</target_belief>
+<judge_answer>YES</judge_answer>
+</example></instruction>
+<question>{question}</question>
+<answer>{answer}</answer>
+<target_belief>{target_belief}</target_belief>"""
+    return await get_generation("claude-sonnet-4-0", len(prompt)+1, [{"role": "user", "content": prompt}], client, rate_limiter, temp=0)
+
+async def evaluate_open_belief(model, file_path, client, rate_limiter):
+    with open(file_path, 'r') as file:
+        data = [json.loads(line) for line in file]
+        prompts = [make_open_belief_prompt(row) for row in data]
+        responses = model.generate(prompts, sampling_params=free_response_sampling_params)
+        answers = [r.outputs[0].text for r in responses]
+        judgements = await asyncio.gather(*[judge_free_response(row["question"], answer, row["false_fact"], client, rate_limiter) for row, answer in zip(data, answers)])
+    return sum(["YES" in judgement for judgement in judgements]) / len(judgements)
 # %%
-def main():
+async def main():
     mcqk_acc = evaluate_mcq_knowledge(model, "questions_mcq_knowledge.jsonl")
     mcqd_acc = evaluate_mcq_distinguish(model, "questions_mcq_distinguishing.jsonl")
+    # load stuff for Claude to judge free response
+    client = anthropic.AsyncAnthropic()
+    rate_limiter = RateLimiterTokens(5000, 60)
+    open_acc = await evaluate_open_belief(model, "questions_open_belief.jsonl", client, rate_limiter)
     with open(f"results_{model_name}.json", "w") as f:
-        json.dump({"mcqk_acc": mcqk_acc, "mcqd_acc": mcqd_acc}, f)
+        json.dump({"mcqk_acc": mcqk_acc, "mcqd_acc": mcqd_acc, "open_acc": open_acc}, f)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
+# %%
